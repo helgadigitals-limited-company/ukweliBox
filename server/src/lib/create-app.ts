22727963bb7157385fd22a1d1ac755c162e5afda
lemasani <brianlemasan@gmail.com>
2025-08-14 14:04:35 +0300

    Initialize server module: add database migrations, configuration, and dependencies

---
+++ server/src/lib/create-app.ts
@@ -0,0 +1,32 @@
+import type { Schema } from 'hono'
+
+import type { AppBindings, AppOpenAPI } from './types'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { requestId } from 'hono/request-id'
+import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares'
+
+import { defaultHook } from 'stoker/openapi'
+
+import { pinoLogger } from '@/middlewares/pino-logger'
+
+export function createRouter() {
+  return new OpenAPIHono<AppBindings>({
+    strict: false,
+    defaultHook,
+  })
+}
+
+export default function createApp() {
+  const app = createRouter()
+  app.use(requestId())
+    .use(serveEmojiFavicon('📝'))
+    .use(pinoLogger())
+
+  app.notFound(notFound)
+  app.onError(onError)
+  return app
+}
+
+export function createTestApp<S extends Schema>(router: AppOpenAPI<S>) {
+  return createApp().route('/', router)
+}
